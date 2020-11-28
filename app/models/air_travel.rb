8adb8eed4e185116d63f1db4af2dfb94c1fc9a75
Adrian Apodaca <aapodaca@gmail.com>
2020-11-27 21:12:40 -0800

    update model constraints for air_travel

--- app/models/air_travel.rb
+++ app/models/air_travel.rb
@@ -11,5 +11,8 @@
 #  updated_at :datetime         not null
 #
 class AirTravel < ApplicationRecord
+    validates :area_code, :month, :passengers, :sales, presence: true
+    validates :area_code, inclusion: { in: (1..4).to_a }
+    validates :month, inclusion: { in: (1..12).to_a }
     
 end
